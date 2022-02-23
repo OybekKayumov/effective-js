43d28fdfc33f381abb8465a4edd6846b5c530b8f
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-02-23 20:35:45 +0500

    add two buttons into Section.js

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,7 +4,19 @@ import styled from 'styled-components'
 function Section() {
   return (
     <Wrap>
-      Section
+      <ItemText>
+        <h1>Model S</h1>
+        <p>Order Online for Touchless Delivery</p>
+      </ItemText>
+
+      <ButtonGroup>
+          <LeftButton>
+              Custom Order
+          </LeftButton>
+          <RightButton>
+              Existing Inventory
+          </RightButton>
+      </ButtonGroup>      
     </Wrap>
   )
 }
@@ -18,5 +30,38 @@ const Wrap = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   background-image: url('/images/model-s.jpg');
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+`
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+const ButtonGroup = styled.div`
+      display: flex;
+      margin-bottom: 30px;
+      
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 14px;
+    cursor: pointer;
+
+`
+
+const RightButton = styled(LeftButton)`
 
 `
